90c5817378b1d492458bb82c96ef9d7a4114bdc7
Sami Repo <sami.repo@valamis.com>
2019-10-22 16:19:48 +0300

    Added checks for finding items.

--- src/Data/DataParser.js
+++ src/Data/DataParser.js
@@ -1,8 +1,10 @@
 import armors from './ShadowPanther_ArmorsPVE.json';
 import weapons from './ShadowPanther_WeaponsPVE.json';
 import enchants from './ShadowPanther_Enchants.json';
-import { enchantSlots } from './Slots';
-import { weaponSlots } from './Slots';
+
+import itemSlots, { enchantSlots, weaponSlots } from './Slots';
+import { is } from '@babel/types';
+import { isFulfilled } from 'q';
 
 export function idFor(slot){
     if(slot === "RING_1" || slot === "RING_2"){
@@ -12,20 +14,42 @@ export function idFor(slot){
     }else {
         return slot;
     }
-}
+};
+
+function hasSlot(slots, slot){
+    var has = false; 
+    slots.map( eSlot => {
+        if(eSlot === slot){
+            has = true;
+        }
+    });
+    return has; 
+};
+
 
 export function itemById(slot, id){
-    const slotID = idFor(slot);
-    return armors[slotID][id];
+    if(hasSlot(itemSlots, slot)){
+        const slotID = idFor(slot);
+        return armors[slotID][id];
+    }
 }
 
 export function weapon(slot, id){
-    return weapons[slot][id];
+    if(hasSlot(weaponSlots, slot)){
+        return weapons[slot][id];
+    }
 }
 
 export function enchant(slot, id){
-    return enchants[slot][id];
-    
+
+    if(hasSlot(enchantSlots, slot)){
+        return enchants[slot][id];
+    }
+
+}
+
+export function weaponsFor(slot){
+    return weapons[slot];
 }
 
 export function enchantsFor(slot){
